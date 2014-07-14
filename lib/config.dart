70e7a95b51e703ec1445d3210d27856ef9ff5fe9
matystl <mfedak@gmail.com>
2014-07-14 14:54:28 +0200

    added lib

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+library clean_my_admin.config;
+
+
+final List allCollection = const ['player', 'club', 'user', 'match',
+  'round', 'user_rank', 'bucket_user', 'transaction',
+  'player_archive', 'psc_buckets', 'bucket_size', 'bucket_competition_info'];
